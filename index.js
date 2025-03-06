fec2669e65ae33e3f73c4a522a78a27dfae6d984
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-06 20:38:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  const hqBlock = 42;
+  return Math.abs(block - hqBlock);
+}
+
+
+
+function distanceFromHqInFeet(feet) {
+
+}
+
+function distanceTravelledInFeet(feet) {
+
+}
+
+function calculatesFarePrice(price) {
+
+}
